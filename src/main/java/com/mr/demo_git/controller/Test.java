9d0d3df8b3ecb05820270f6ea742df264a050dbc
ruochena <1131484425@qq.com>
2019-10-25 13:52:20 +0800

    project init

---
+++ src/main/java/com/mr/demo_git/controller/Test.java
@@ -0,0 +1,10 @@
+package com.mr.demo_git.controller;
+
+public class Test {
+
+
+    public String test(){
+        return "测试";
+    }
+
+}
---
+++ src/test/java/com/mr/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
